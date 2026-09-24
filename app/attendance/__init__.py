80ddf9750ae1ea71ca7c3cddb47258fea155a2bb
mohamedrz25 <mohamed3211r@gmail.com>
2026-09-24 17:00:31 +0530

    Connect attendance to Flask

--- app/attendance/__init__.py
+++ app/attendance/__init__.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Flask
 
-attendance_bp = Blueprint(
-    "attendance",
-    __name__,
-    url_prefix="/attendance"
-)
+from .attendance import attendance_bp
+from .attendance import routes
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(attendance_bp)
+
+    return app
